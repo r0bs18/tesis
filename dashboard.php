f084f028e6a1f03cf518edc513f3e0486765b5ae
Wilmer Blanco <wilmervzla2000@gmail.com>
2017-06-26 00:29:36 -0300

    cambios

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,8 @@
-<?php include("includes/nav_bar.php"); ?>
-<div class="flex_dashboard_item dashboard_container">
-	<h1>dasadasd asdasd</h1>
+<div class="flex_dashboard">
+  <?php include("includes/head.php") ?>
+  <?php include("includes/nav_bar.php"); ?>
+	<div class="flex_dashboard_item dashboard_container">
+		<h1>dasadasd asdasd</h1>
+	</div>
 </div>
+<?php include("includes/footer_script.php") ?>
